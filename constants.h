8096ef61cb5bde67c8d49913584272cd6181f534
Alyn <alyn_509@yahoo.com>
2015-05-06 21:36:03 +0300

    updated wheel distances

--- constants.h
+++ constants.h
@@ -2,7 +2,7 @@
 #define little_robot_constants_h
 
 const int encoderResolution = 1500;
-const float wheelDiameter = 10.5;
-const float distanceBetweenWheels = 15.3; //valoare interioara
+const float wheelDiameter = 9.2;
+const float distanceBetweenWheels = 15.8; //valoare interioara
 
 #endif
